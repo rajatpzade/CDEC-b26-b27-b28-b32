4417fcd2316b28180f0d6133f6b621ff729522bc
Rajat <rajatz@gmail.com>
2024-03-22 07:20:44 +0530

    --- Auto Git Commit By Rajatz ---

--- docker.sh
+++ docker.sh
@@ -16,4 +16,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-#
+# To install the latest version, run:
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+

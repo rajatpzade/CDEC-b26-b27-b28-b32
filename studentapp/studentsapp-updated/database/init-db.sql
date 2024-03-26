7dea32536c87dd5297de26ed58ce74cff991279f
Rajat <rajatz@gmail.com>
2024-03-26 11:12:28 +0530

    --- Auto Git Commit By Rajatz ---

---
+++ studentapp/studentsapp-updated/database/init-db.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS studentapp;
+
+USE studentapp;
+
+CREATE TABLE IF NOT EXISTS students (
+    student_id INT NOT NULL AUTO_INCREMENT,
+    student_name VARCHAR(100) NOT NULL,
+    student_addr VARCHAR(100) NOT NULL,
+    student_age VARCHAR(3) NOT NULL,
+    student_qual VARCHAR(20) NOT NULL,
+    student_percent VARCHAR(10) NOT NULL,
+    student_year_passed VARCHAR(10) NOT NULL,
+    PRIMARY KEY (student_id)
+);

a2c7549893ee57dbb830ff19ffe0f62b6fc565ed
Rajat <rajatz@gmail.com>
2024-05-22 09:39:08 +0530

    --- Auto Git Commit By Rajatz ---

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage('Build') { 
             steps {
-                // 
+               echo 'successfully build code '
             }
         }
         stage('Test') { 
